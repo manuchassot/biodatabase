864dec4ee103158a8617f2f117844791011cdf5d
manu Chassot <manu.chassot@gmail.com>
2020-01-20 06:58:55 +0300

    addition of code of metadata loading

---
+++ load/running_load_metadata_reference_tables.R
@@ -0,0 +1,20 @@
+### Libraries
+library(rmarkdown)  #function render
+library(prettydoc)  #function prettydoc
+library(RPostgreSQL,quietly = TRUE)
+
+# Path
+wd <- '/home/stagiaire/Emotion3/R/'
+setwd(wd)
+
+# Swith to English for date format in title
+Sys.setlocale("LC_TIME", 'en_GB.UTF-8')
+
+# Step 1: Load metadata ----
+
+### PDF
+render('./load_metadata.Rmd',output_format = pdf_document(highlight = "espresso",keep_tex = FALSE))
+
+### HTML PRETTY
+render('./load_metadata.Rmd',output_format = html_pretty(theme="leonids",toc=TRUE))
+
