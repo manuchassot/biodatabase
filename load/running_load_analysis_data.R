f9e184625047b85f2d2dba18d5ee486ccd67563f
manu Chassot <manu.chassot@gmail.com>
2020-01-20 10:01:43 +0300

    update of load_metadata & addition of load_analysis_data

---
+++ load/running_load_analysis_data.R
@@ -0,0 +1,18 @@
+### Libraries
+library(rmarkdown)  #function render
+library(prettydoc)  #function prettydoc
+library(RPostgreSQL,quietly = TRUE)
+
+# Path
+wd <- '/home/stagiaire/Emotion3/R/'
+setwd(wd)
+
+# Swith to English for date format in title
+Sys.setlocale("LC_TIME", 'en_GB.UTF-8')
+
+### PDF
+render('./load_analysis_data.Rmd',output_format = pdf_document(highlight = "espresso",keep_tex = FALSE))
+
+### HTML PRETTY
+render('./load_analysis_data.Rmd',output_format = html_pretty(theme="leonids",toc=TRUE))
+
