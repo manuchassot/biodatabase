01a4674f26b41b22b6b55c47fd399022008956d9
manu Chassot <manu.chassot@gmail.com>
2020-01-30 18:10:23 +0300

    addition of sql code to add 2 references tables in the database that were not present in the most recent dump

---
+++ load/add_new_reference_table_for_minerals.sql
@@ -0,0 +1,16 @@
+CREATE TABLE references_tables.minerals (
+
+    c_tm character varying(255) NOT NULL,
+    l_tm character varying(255),
+    c_tm_grp1 character varying(255),
+    l_tm_grp1 character varying(255),
+    c_tm_grp2 character varying(255),
+    l_tm_grp2 character varying(255),
+    Remark character varying(255)
+    
+);
+
+ALTER TABLE references_tables.minerals OWNER TO "dbaEmotion";
+
+
+
---
+++ load/add_new_reference_table_for_organic_contaminants.sql
@@ -0,0 +1,17 @@
+CREATE TABLE references_tables.organic_contaminants (
+
+    c_oc character varying(255) NOT NULL,
+    l_oc1 character varying(255),
+    l_oc2 character varying(255),
+    c_oc_grp1 character varying(255),
+    l_oc_grp1 character varying(255),
+    c_oc_grp2 character varying(255),
+    l_oc_grp2 character varying(255)
+    
+);
+
+ALTER TABLE references_tables.organic_contaminants OWNER TO "dbaEmotion";
+
+
+
+
