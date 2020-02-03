eaa4307933a529186521911c0d58b7ac27473fc8
manu Chassot <manu.chassot@gmail.com>
2020-02-03 08:26:02 +0300

    update of reference table vessel storage

---
+++ sql/update_reference_table_for_vessel_storage.sql
@@ -0,0 +1,8 @@
+﻿DROP TABLE references_tables.vessel_storage;
+CREATE TABLE references_tables.vessel_storage
+(
+  c_bat_stor_mod character varying(255),
+  desc_bat_stor_mod_fr character varying(255),
+  desc_bat_stor_mod_uk character varying(255) NOT NULL
+);
+ALTER TABLE references_tables.vessel_storage OWNER TO "dbaEmotion";
