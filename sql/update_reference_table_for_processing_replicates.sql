657e2a613cb855172ce6b4c8da6d7ffc14d96042
manu Chassot <manu.chassot@gmail.com>
2020-02-03 07:46:09 +0300

    update of some reference tables formats and structure

---
+++ sql/update_reference_table_for_processing_replicates.sql
@@ -0,0 +1,9 @@
+﻿DROP TABLE IF EXISTS references_tables.processing_replicates;
+
+CREATE TABLE references_tables.processing_replicates
+(
+  l_process_rep character varying(255),
+  desc_process_rep character varying(255)
+);
+
+ALTER TABLE references_tables.operator OWNER TO "dbaEmotion";
---
+++ sql/update_reference_table_for_stomach_fulness.sql
@@ -0,0 +1,9 @@
+﻿DROP TABLE references_tables.stomach_fullness;
+
+CREATE TABLE references_tables.stomach_fullness
+(
+  c_stom_fullness character varying(255) NOT NULL,
+  desc_stom_fullness character varying(255)
+);
+
+ALTER TABLE references_tables.stomach_fullness OWNER TO "dbaEmotion";
