8e7baf6669716c5000bfe7d40a3d06de7b80d8f1
Michael Wagner <mwagner@allspatial.info>
2021-08-06 07:45:44 +0200

    Install missing packages automatically when running the scripts

--- etl/r/1_analysis_core_and_metadata2csv_and_pg.R
+++ etl/r/1_analysis_core_and_metadata2csv_and_pg.R
@@ -6,7 +6,7 @@ package.check <- lapply(
   packages,
   FUN = function(x) {
     if (!require(x, character.only = TRUE)) {
-      install.packages(x, dependencies = TRUE)
+      install.packages(x, dependencies = TRUE, repos = "http://cran.us.r-project.org")
       library(x, character.only = TRUE)
     }
   }
